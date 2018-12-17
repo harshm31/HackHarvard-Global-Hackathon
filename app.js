1010fbe816462f16750557cbbfee1448e9058d62
Rahul <skvrahul@gmail.com>
2018-12-17 06:19:29 +0000

    Adds route /monthMapping

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  * Link to documentation for connection string - https://docs.mongodb.com/manual/reference/connection-string/
  * If the mongodb server is running on the default port on localhost then the given connection string should work fine.
  * **/
+
 var express = require('express')
 var mongo = require('mongojs')
 var app = express()
@@ -11,22 +12,19 @@ connection_string = 'climatedb'
 var db = mongo(connection_string, ['climate'])
 
 //Port to run the web server on
-const PORT = 3000
+const PORT = 8080
 const requestValidator = function(req, res, next){
 	if(req.method != 'GET'){
 		res.status(405)
 		res.send({'error':'This method is currently not supported by this server'})
 	}else{
-		if('city' in req.query && req.query['city'] != null && req.query['city'] != undefined){
-			next()	
-		}else{
-			res.status(422)	
-			res.send({'error':'Invalid parameters'})
-		}	
+		next()
 	}	
-
 }
 app.use(requestValidator)
+
+
+const monthMapping = {'Mar': 3, 'Feb': 2, 'Aug': 8, 'Apr': 4, 'June': 6, 'Jan': 1, 'May':5, 'Nov': 11, 'July': 7, 'Dec': 12, 'Sept': 9, 'Oct': 10, 'Year':-1}
 
 
 /** route: GET /climateData
@@ -34,16 +32,26 @@ app.use(requestValidator)
  *  localhost:3000/climateData?city=agra
  * **/
 app.get('/climateData', function(req, res){
-	const city = req.query.city
+	const city = req.query.city.toLowerCase()
 	db.climate.find({'city':city}, function(err, docs){
-		if(docs.size != 0){
-			res.send(docs)	
-		}else{
+		if( docs==undefined || docs.size == 0){
+			console.log(err)
 			res.status(204)
 			res.end()
+		}else{
+			res.send(docs)	
 		}
 	})
 })
+
+/** route: GET /monthMapping
+ *  Returns a mapping between month_name('string') and month_num('int')
+ * **/
+app.get('/monthMapping', function(req, res){
+	res.send(monthMapping)
+})
+
+
 
 app.listen(PORT, function(){
 	console.log('App is running on Port number ' + PORT)
