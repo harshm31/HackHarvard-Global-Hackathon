123d462d4912195de3abb6a808aff795c57998c6
harshm31 <harshmutha31@gmail.com>
2018-12-22 00:21:33 +0530

    small changes to climate news script

--- ClimateNews/scrap_news.py
+++ ClimateNews/scrap_news.py
@@ -6,21 +6,10 @@ import os
 import shutil
 import news_json_parser as njp
 
-cwd = os.getcwd()
-#get today's date 
-date1 = str(datetime.now()).split(' ')[0]
-dt = datetime.strptime(date1, '%Y-%m-%d')
-yr = dt.year%100
-m = dt.month
-d = dt.day
-folder_name = str(d)+str(m)+str(yr)
-print(folder_name)
-
 #code to scrape data from website 
 url = "https://insideclimatenews.org/todaysclimate"
 req = requests.get(url , verify = False)
 soup = BeautifulSoup(req.content,"lxml")
-print(soup)
 
 try :
     output=njp.get_json_output(soup)
