acb368e7566d5b922613cecc4274a645c394a807
harshm31 <harshmutha31@gmail.com>
2018-12-22 00:30:20 +0530

    added code to flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,13 +1,29 @@
 from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
+import requests
+import sys
+sys.path.insert(0,'../ClimateNews')
+import news_json_parser as njp 
 import os 
+
 app = Flask(__name__)
-@app.route('/getData', methods=['GET'])
+@app.route('/getNews', methods=['GET'])
 def parse_request():
-    for key in request.args:
-        value = request.args[key]
-        print(key+ ' - '+ value)
-        ob ={'foo':'bar'} 
-    return jsonify(ob) 
+    #for key in request.args:
+     #   value = request.args[key]
+      #  print(key+ ' - '+ value)
+       # ob ={'foo':'bar'} 
+    #return jsonify(ob)
+    #code to scrape data from website 
+    url = "https://insideclimatenews.org/todaysclimate"
+    req = requests.get(url , verify = False)
+    soup = BeautifulSoup(req.content,"lxml")
+
+    try :
+        output=njp.get_json_output(soup)
+        return output
+    except Exception as e :
+        return e
 if __name__ == "__main__":
     server_port = os.environ['SERVER_PORT']
     print(server_port)
